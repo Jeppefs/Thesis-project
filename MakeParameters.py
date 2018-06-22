Fixed the mistake of spaces in mean and variance.

MakeParameters.py
@@ -83,20 +83,20 @@ def MakeEmptyListFromDict(aDict):
 def MakeParametersAndSettings():
 
     folder = "data"
-    name = "simpleDeath"
+    name = "VaryAntigen"
 
     length = 1
     width = 1
     parameters = OrderedDict()
     
     parameters["NHosts"] = np.array([10000])
-    parameters["InfectionSpeed"] = np.array([1.0])
+    parameters["InfectionSpeed"] = np.array([0.995])
     parameters["ImmunitySpeed"] = np.array([1.0])
-    parameters["DeathSpeed"] = np.arange(0.0, 0.01+np.finfo(float).eps, 0.0001)
+    parameters["DeathSpeed"] = np.arange(0.001)
     parameters["MutationSpeed"] = np.array([0.00])
 
     parameters['NAntigens'] = np.array([1])
-    parameters['MaxAntigenValue'] = np.array([1])
+    parameters['MaxAntigenValue'] = np.array([i+1 for i in range(20)])
     parameters['InitialInfected'] = np.array([100])
 
     parameters['IsMultipleInfectionsPossible'] = np.array(["true"])
@@ -104,7 +104,7 @@ def MakeParametersAndSettings():
     settings = OrderedDict()
     settings["Runs"] = [25000000] 
     settings["BurnIn"] = [0]
-    settings["Repeat"] = [1]
+    settings["Repeat"] = [10]
     settings["ShouldSaveData"] = ["true"]
     settings["ShouldSaveDataWhileRunning"] = ["true"] 
     settings["ShouldCreateNewDataFile"] = ["true"]

plotting.py
@@ -28,6 +28,7 @@ class MalariaStatistics():
         plt.savefig(figName)
         return
 
+    # Creates a plot of the mean and variance. 
     def PlotMeanInfection(self, vary):
         plt.figure()
         plt.errorbar(self.parameters[vary], self.dataEnd[" mean"], np.sqrt(self.dataEnd[" variance "]))
@@ -37,11 +38,13 @@ class MalariaStatistics():
         plt.savefig(figName)
         return
 
-    # Creates a plot of the mean and variance. 
-    def PlotMeanAndVariance(self):
+    # Recalculates dataEnd, such that it finds the mean and variance from the repeat cases. 
+    def GetMeanAndVarianceFromRepat(self):
+        for i in range(self.parameters["Repeat"]):
+            
         return
 
-q = MalariaStatistics("simpleDeath")
+q = MalariaStatistics("simpleDeathRepeat")
 q.PlotExtinctionTime("DeathSpeed")
 print(q.dataEnd.keys())
 q.PlotMeanInfection("DeathSpeed")