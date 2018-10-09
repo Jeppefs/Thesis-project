affe62eed981ddba47ed904426627551c36ce4a5
Jeppe Sorensen <s.jeppe.f@gmail.com>
2018-10-09 16:23:45 +0200

    Put the differential system into another files and added some comments.

--- runge_kutta/DeterministcModelling.py
+++ runge_kutta/DeterministcModelling.py
@@ -2,84 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib 
 import RungeKutta as RK
+import DifferentialSystems as DS
 from Latexifier import LatexifierFunctions as LF 
 
-# What the fuck does the function do? 
-def SIRPlus(param, values, t):
-    valuesRate = np.zeros(len(values))
-    
-    valuesRate[0] = -param[0] * values[0] * (values[1] + values[3]) + param[1] * values[1] + param[2] * values[3]
-    valuesRate[1] = param[0] * values[0] * (values[1] + values[3]) - param[1] * values[1] - param[3] * values[1] 
-    valuesRate[2] = - param[4] * values[2] * (values[1] + values[3]) + param[3]*values[1] + param[5] * values[3] 
-    valuesRate[3] = param[4] * values[2] * (values[1] + values[3]) - param[2] * values[3] - param[5] * values[3]
 
-    return valuesRate
-
-def SI(param, values, t):
-    valuesRate = np.zeros(len(values))
-
-    valuesRate[0] = - param[0] * values[0] + param[1] * values[1]
-    valuesRate[1] = param[0] * values[0] - param[1] * values[1]
-
-    return valuesRate
-
-#def SIRS(param, values, t):
-#    valuesrate = np.zeros(len(values))
-#
-#    valuesRate[0] = - param[0] * values[0] + param[2] * values[2]
-#    valuesRate[1] = param[0] * values[0] - param[1] * values[1]
-#    valuesRate[2] = param[1] * values[2]  - param[2] * 
-#
-#    return valuesRate 
-
-def Ross(param, values, t):
-    valuesRate = np.zeros(len(values))
-    
-    valuesRate[0] = param[0] * param[1] * param[2] * values[1] * (1 - values[0]) - param[3] * values[0]
-    valuesRate[1] = param[0] * param[4] * values[0] * (1 - values[1]) - param[5] * values[1]
-    return valuesRate
-
-def Deitz(param, values, t):
-    valuesRate = np.zeros(len(values))
-
-    valuesRate[0] = param[0] + param[1] * values[5] - 1
-    valuesRate[1] = param[1]
-    valuesRate[2] = param[2] 
-    valuesRate[3] = 0
-    valuesRate[4] = 0
-    valuesRate[5] = 0
-    valuesRate[6] = 0
-    
-    return valuesRate
-
-def Macdonald(param, values, t):
-    valuesRate = np.zeros(len(values))
-    return valuesRate
-
-def SimpleInfectionSteadyState(param, values, t):
-    valuesRate = np.zeros(len(values))
-
-    valuesRate[0] = param[0]*values[0]*values[1] - param[1]*values[0]
-    valuesRate[1] = - param[0]*values[0]*values[1] + param[1]*values[0]
-    return valuesRate
-
-def SimpleInfection(param, values, t):
-    valuesRate = np.zeros(len(values))
-
-    valuesRate[0] = - param[0] * values[0] * (values[1] + values[2])
-    valuesRate[1] = param[0] * values[0] * (values[1] + values[2]) - param[1] * values[1]
-    valuesRate[2] = param[0] * values[3] * (values[1] + values[2]) + param[1] * values[1] - param[1] * values[2]
-    valuesRate[3] = - param[0] * values[3] * (values[1] + values[2]) + param[1] * values[2] 
-    return valuesRate
-
-def Replacement(p, v, t):
-    valuesRate = np.zeros(len(v))
-
-    valuesRate[0] = - p[0] * v[0] * (v[1] + v[2]) + p[2] * (v[1]+v[2]+v[3])
-    valuesRate[1] = p[0] * v[0] * (v[1] + v[2]) - p[1] * v[1] - p[2] * v[1] 
-    valuesRate[2] = p[0] * v[3] * (v[1] + v[2]) + p[1] * v[1] - p[1] * v[2] - p[2] * v[2]
-    valuesRate[3] = - p[0] * v[3] * (v[1] + v[2]) + p[1] * v[2]  - p[2] * v[3]
-    return valuesRate
 
 def PlotSimple(func, initial, param, legend, runs = 50000, xlabel = "", ylabel = "", color = None):
     q = RK.RungeKutta(initialConditions = initial, equation = func, param = param, dt = 0.01)
@@ -179,11 +105,11 @@ def ReplacementRastaScan():
 LF.Latexify(fig_width = 12.65076*0.49, label_size = [1.05, 1.05])
 
 """Simple function and plotting"""
-#PlotSimple(func = SimpleInfection, initial = [0.99, 0.01, 0.0, 0.0], param = [0.9, 1.0], legend = ["$S$", "$I$", "$I_R$", "$S_R$"], runs=10000, xlabel="Iteration", ylabel="Proportion")
-#PlotSimple(func = SimpleInfectionSteadyState, initial = [0.99, 0.01], param = [1.1, 1.0], legend = ["$I_R$", "$S_R$"], runs=10000, xlabel="Iteration", ylabel="Proportion", color = ['g','r'])
+#PlotSimple(func = DS.SimpleInfection, initial = [0.99, 0.01, 0.0, 0.0], param = [0.9, 1.0], legend = ["$S$", "$I$", "$I_R$", "$S_R$"], runs=10000, xlabel="Iteration", ylabel="Proportion")
+#PlotSimple(func = DS.SimpleInfectionSteadyState, initial = [0.99, 0.01], param = [1.1, 1.0], legend = ["$I_R$", "$S_R$"], runs=10000, xlabel="Iteration", ylabel="Proportion", color = ['g','r'])
 
 """Replacement"""
-PlotSimple(Replacement, initial = [0.99, 0.01, 0.0, 0.0], param = [0.95, 1.0, 0.1], legend = ["S","I","$I_R$", "$S_R$"], runs=10000, xlabel="Iteration", ylabel="Proportion")
+PlotSimple(DS.Replacement, initial = [0.99, 0.01, 0.0, 0.0], param = [0.95, 1.0, 0.1], legend = ["S","I","$I_R$", "$S_R$"], runs=10000, xlabel="Iteration", ylabel="Proportion")
 
 """Ross"""
 #PlotSimple(func = Ross, initial = [0.1, 0.1], param = [0.09, 0.2, 2.0, 0.01, 0.5, 0.1], legend = ["$I_h$","$I_m$"], runs=100000, xlabel="Iterations", ylabel="Proportion infected")
---
+++ runge_kutta/DifferentialSystems.py
@@ -0,0 +1,68 @@
+import numpy as np 
+
+def SIRPlus(param, values, t):
+    valuesRate = np.zeros(len(values))
+    
+    valuesRate[0] = -param[0] * values[0] * (values[1] + values[3]) + param[1] * values[1] + param[2] * values[3]
+    valuesRate[1] = param[0] * values[0] * (values[1] + values[3]) - param[1] * values[1] - param[3] * values[1] 
+    valuesRate[2] = - param[4] * values[2] * (values[1] + values[3]) + param[3]*values[1] + param[5] * values[3] 
+    valuesRate[3] = param[4] * values[2] * (values[1] + values[3]) - param[2] * values[3] - param[5] * values[3]
+
+    return valuesRate
+
+def SI(param, values, t):
+    valuesRate = np.zeros(len(values))
+
+    valuesRate[0] = - param[0] * values[0] + param[1] * values[1]
+    valuesRate[1] = param[0] * values[0] - param[1] * values[1]
+
+    return valuesRate
+
+def Ross(param, values, t):
+    valuesRate = np.zeros(len(values))
+    
+    valuesRate[0] = param[0] * param[1] * param[2] * values[1] * (1 - values[0]) - param[3] * values[0]
+    valuesRate[1] = param[0] * param[4] * values[0] * (1 - values[1]) - param[5] * values[1]
+    return valuesRate
+
+def Deitz(param, values, t):
+    valuesRate = np.zeros(len(values))
+
+    valuesRate[0] = param[0] + param[1] * values[5] - 1
+    valuesRate[1] = param[1]
+    valuesRate[2] = param[2] 
+    valuesRate[3] = 0
+    valuesRate[4] = 0
+    valuesRate[5] = 0
+    valuesRate[6] = 0
+    
+    return valuesRate
+
+def Macdonald(param, values, t):
+    valuesRate = np.zeros(len(values))
+    return valuesRate
+
+def SimpleInfectionSteadyState(param, values, t):
+    valuesRate = np.zeros(len(values))
+
+    valuesRate[0] = param[0]*values[0]*values[1] - param[1]*values[0]
+    valuesRate[1] = - param[0]*values[0]*values[1] + param[1]*values[0]
+    return valuesRate
+
+def SimpleInfection(param, values, t):
+    valuesRate = np.zeros(len(values))
+
+    valuesRate[0] = - param[0] * values[0] * (values[1] + values[2])
+    valuesRate[1] = param[0] * values[0] * (values[1] + values[2]) - param[1] * values[1]
+    valuesRate[2] = param[0] * values[3] * (values[1] + values[2]) + param[1] * values[1] - param[1] * values[2]
+    valuesRate[3] = - param[0] * values[3] * (values[1] + values[2]) + param[1] * values[2] 
+    return valuesRate
+
+def Replacement(p, v, t):
+    valuesRate = np.zeros(len(v))
+
+    valuesRate[0] = - p[0] * v[0] * (v[1] + v[2]) + p[2] * (v[1]+v[2]+v[3])
+    valuesRate[1] = p[0] * v[0] * (v[1] + v[2]) - p[1] * v[1] - p[2] * v[1] 
+    valuesRate[2] = p[0] * v[3] * (v[1] + v[2]) + p[1] * v[1] - p[1] * v[2] - p[2] * v[2]
+    valuesRate[3] = - p[0] * v[3] * (v[1] + v[2]) + p[1] * v[2]  - p[2] * v[3]
+    return valuesRate
